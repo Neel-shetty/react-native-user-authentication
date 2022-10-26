10313f86a76be55be03353d92c99af19ab86a5b4
Neel <neelnarayanshetty@gmail.com>
2022-10-26 12:43:11 +0530

    User Auth

---
+++ screens/SignupScreen.js
@@ -0,0 +1,38 @@
+import { useFocusEffect } from "@react-navigation/native";
+import { useContext, useState } from "react";
+import AuthContent from "../components/Auth/AuthContent";
+import { createUser } from "../util/auth";
+import LoadingOverlay from "../components/ui/LoadingOverlay";
+import { Alert } from "react-native";
+import { AuthContext } from "../store/auth-context";
+
+function SignupScreen() {
+  const [isAuthenticating, setIsAuthenticating] = useState(false);
+
+  const authCtx = useContext(AuthContext)
+
+  async function signupHandler({ email, password }) {
+    setIsAuthenticating(true);
+    try {
+      const token = await createUser(email, password);
+      authCtx.authenticate(token)
+    } catch (error) {
+      console.log(error)
+      Alert.alert(
+        "Authentication failed",
+        "Could not create account. Please check your input or Try again later"
+      );
+      setIsAuthenticating(false);
+    }
+
+    
+  }
+
+  if (isAuthenticating) {
+    return <LoadingOverlay message="Creating Account..." />;
+  }
+
+  return <AuthContent onAuthenticate={signupHandler} />;
+}
+
+export default SignupScreen;
